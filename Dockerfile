07523412be3c6c59800312dde0b23961a87a4375
guychienll <guychienll@gmail.com>
2024-03-03 19:05:41 +0800

    fix: App lack of prop types

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM node:20.10.0
+
 WORKDIR /app
 COPY . .
+
 RUN ["npm","install"]
+RUN ["npm","run","build"]
+
 CMD ["npm","run","start"]
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import "@/styles/globals.css";
+import PropTypes from "prop-types";
+
+App.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    pageProps: PropTypes.object.isRequired,
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+    return <Component {...pageProps} />;
 }
